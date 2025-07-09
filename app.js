bae271876a1b6363083de9ff7b4f9da1db380391
kbrsrkl1 <kubranur216@gmail.com>
2025-07-10 00:34:46 +0300

    commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const formEl = document.querySelector(".form")
+
+const inputEl = document.querySelector(".input")
+
+const ulEl = document.querySelector(".list")
+
+formEl.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    toDoList()
+})
+
+function toDoList(){
+    let newTask = inputEl.value;
+    const liEl = document.createElement("li")
+    liEl.innerText = newTask;
+    ulEl.appendChild(liEl)
+    inputEl.value = ""
+    const checkBtnEl = document.createElement("div")
+    checkBtnEl.innerHTML = `
+    <i class="fa-solid fa-square-check">
+    `
+    liEl.appendChild(checkBtnEl)
+    const squareEl = document.createElement("div")
+    squareEl.innerHTML = `
+    <i class="fa-solid fa-trash"></i>
+    `
+    liEl.appendChild(squareEl)
+    
+    checkBtnEl.addEventListener("click" , () => {
+        liEl.classList.toggle("checked")
+    })
+
+
+    squareEl.addEventListener("click", () => {
+        liEl.remove()
+    })
+
+
+}

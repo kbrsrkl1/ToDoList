106a9ab63bda3a4a759f28b02655f1fad4168dfa
kbrsrkl1 <kubranur216@gmail.com>
2025-07-10 13:37:53 +0300

    commit

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ const inputEl = document.querySelector(".input")
 
 const ulEl = document.querySelector(".list")
 
+let list = JSON.parse(localStorage.getItem("list"))
+
+list.forEach(task => {
+    toDoList(task)
+})
+
 formEl.addEventListener("submit", (event)=> {
     event.preventDefault();
     toDoList()
 })
 
-function toDoList(){
+function toDoList(task){
     let newTask = inputEl.value;
+    if(task){
+        newTask = task.name
+    }
+
     const liEl = document.createElement("li")
+    if(task && task.checked) {
+        liEl.classList.add("checked")
+    }
+
     liEl.innerText = newTask;
     ulEl.appendChild(liEl)
     inputEl.value = ""
@@ -25,24 +39,24 @@ function toDoList(){
     liEl.appendChild(squareEl)
     checkBtnEl.addEventListener("click" , () => {
         liEl.classList.toggle("checked")
+        updateLocalStorage()
     })
 
     squareEl.addEventListener("click", () => {
-        liEl.remove()
+        liEl.remove();
+        updateLocalStorage();
         });
     updateLocalStorage()
 }
 
-
-    
 function updateLocalStorage() {
     const liEls = document.querySelectorAll("li");
     const list = [];
 
-    liEls.forEach(liEl => {
+    liEls.forEach((liEl) => {
         list.push({
             name: liEl.innerText,
-            checked: liEl.classList.contains("checked")
+            checked: liEl.classList.contains("checked"),
         });
     });
 
